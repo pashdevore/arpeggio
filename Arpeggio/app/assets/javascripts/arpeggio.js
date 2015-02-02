d77e848834553911188c6ac1c660585b3737954a
Pash DeVore <pashdevore@gmail.com>
2015-02-01 21:06:54 -0800

    added backbone folder structure

---
+++ Arpeggio/app/assets/javascripts/arpeggio.js
@@ -0,0 +1,13 @@
+window.Arpeggio = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Arpeggio.initialize();
+});
