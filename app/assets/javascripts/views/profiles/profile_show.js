de29385d0f70f1d4bb3c5ce4d142495417981dea
Pash DeVore <pashdevore@gmail.com>
2015-02-06 18:01:30 -0800

    day four of final project

--- app/assets/javascripts/views/profiles/profile_show.js
+++ app/assets/javascripts/views/profiles/profile_show.js
@@ -23,5 +23,6 @@ Arpeggio.Views.ProfileShow = Backbone.View.extend({
 
     //get user-id data attribute from html button
     var user_id = $target.data('user-id');
+    //TODO: work on method for creating a subscription
   }
 });
